480f65743d9c210c2a06d1d917721e07a6cb80d8
Avery Bentz <averybentz@bell.net>
2015-07-21 13:51:35 -0400

    adding obj c classes for OpenInChromeController.h/.m

---
+++ Open In Chrome Controller/OpenInChromeController.m
@@ -0,0 +1,9 @@
+//
+//  OpenInChromeController.m
+//  Open In Chrome Controller
+//
+//  Created by Avery Bentz on 2015-07-21.
+//  Copyright (c) 2015 Avery Bentz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
